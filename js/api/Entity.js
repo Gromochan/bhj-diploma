2b34d32bccf0dd1674c7493116561007af8ae07c
Ivan Petrov <kepocka@gmail.com>
2019-05-20 17:49:05 +0300

    ver. 0.0.1

--- js/api/Entity.js
+++ js/api/Entity.js
@@ -1,3 +1,4 @@
+import createRequests from '../createRequests.js'
 /**
  * Класс Entity - базовый для взаимодействия с сервером.
  * Имеет свойство URL, равно пустой строке.
@@ -5,46 +6,69 @@
  * */
 class Entity {
 
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static list( data, callback = f => f ) {
+    /**
+     * Запрашивает с сервера список данных.
+     * Это могут быть счета или доходы/расходы
+     * (в зависимости от того, что наследуется от Entity)
+     * */
+    static list(data, callback = f => f) {
+        crateRequest({
+            method: "GET",
+            url: Entity.HOST + Entity.URL,
+            data: data,
+            responseType: "json"
+        })
+    }
 
-  }
+    /**
+     * Создаёт счёт или доход/расход с помощью запроса
+     * на сервер. (в зависимости от того,
+     * что наследуется от Entity)
+     * */
+    static create(data, callback = f => f) {
+        const modData = data;
+        modData._method = "PUT"
+        createRequest({
+            method: "POST",
+            url: Entity.HOST + Entity.URL,
+            responseType: "json",
+            data: modData
+        })
+    }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
-  static create( data, callback = f => f ) {
+    /**
+     * Получает информацию о счёте или доходе/расходе
+     * (в зависимости от того, что наследуется от Entity)
+     * */
+    static get(id = '', data, callback = f => f) {
+        crateRequest({
+            url: Entity.HOST + Entity.URL + '/' + id,
+            data: data,
+            method: "GET",
+            responseType: "json"
+        })
+    }
 
-  }
+    /**
+     * Обновляет информацию о счёте или доходе/расходе
+     * (в зависимости от того, что наследуется от Entity)
+     * */
+    static update(id = '', data, callback = f => f) {
 
-  /**
-   * Получает информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static get( id = '', data, callback = f => f ) {
+    }
 
-  }
-
-  /**
-   * Обновляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static update( id = '', data, callback = f => f ) {
-
-  }
-
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static remove( id = '', data, callback = f => f ) {
-
-  }
+    /**
+     * Удаляет информацию о счёте или доходе/расходе
+     * (в зависимости от того, что наследуется от Entity)
+     * */
+    static remove(id = '', data, callback = f => f) {
+        const modData = data;
+        data._method = "DELETE";
+        crateRequest({
+            url: Entity.HOST + Entity.URL + '/' + id,
+            data: modData,
+            method: "POST",
+            responseType: "json"
+        })
+    }
 }
-
--- js/api/createRequest.js
+++ js/api/createRequest.js
@@ -3,5 +3,24 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest;
+    xhr.withCredentials = true;
+    if (options.method == "GET") {
+        xhr.open(options.method, `${options.url}?${options.data.mail}&${options.data.password}`);
+        xhr.send();
+    } else {
+        cosnt formData = new FormData;
+        formData.append("password", options.data.password);
+        formData.append("mail", options.data.mail);
+        xhr.open(options.method, options.url);
+        xhr.send(formData);
+    }
 
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            options.callback(null, xhr.response);
+        } else {
+            options.callback(xhr.status, xhr.response);
+        }
+    }
 };
